8f8e7283a735c9a2f5b43de20eeea129c2d318b5
Sergey Malyuga <sergey@malyuga.ru>
2026-07-09 15:57:03 +0300

    build(app.config): подключить Effects, HttpClient, Store

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,19 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+import {provideStore} from '@ngrx/store';
+import {appReducer} from './store/app/app.reducer';
+import {provideEffects} from '@ngrx/effects';
+import {OfferEffects} from './store/offer/effects/offer.effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({eventCoalescing: true}),
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideStore(appReducer),
+    provideEffects(OfferEffects)
+  ]
 };
