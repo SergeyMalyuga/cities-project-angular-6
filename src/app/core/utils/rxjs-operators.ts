2ea6fccddd6895f22dd8721c55cd339c96b35bd2
Sergey Malyuga <sergey@malyuga.ru>
2026-07-09 15:46:07 +0300

    feat(rxjs-operators): добавить pipeable операторы для http запросов

---
+++ src/app/core/utils/rxjs-operators.ts
@@ -0,0 +1,20 @@
+import {catchError, MonoTypeOperatorFunction, retry, throwError, timeout, timer} from 'rxjs';
+import {RETRY_ATTEMPTS, TIMEOUT_MS} from '../constants/const';
+import {HttpErrorResponse} from '@angular/common/http';
+import {httpErrorHandler} from './http-error-hadler';
+
+export function getDefaultHttpPipes<T>(): [MonoTypeOperatorFunction<T>, MonoTypeOperatorFunction<T>, MonoTypeOperatorFunction<T>] {
+  return [
+    timeout(TIMEOUT_MS),
+    retry({
+      count: RETRY_ATTEMPTS,
+      delay: (error: HttpErrorResponse) => {
+        if (error.status === 0 || error.status >= 500) {
+          return timer(1500);
+        }
+        return throwError(() => error)
+      },
+    }),
+    catchError(httpErrorHandler)
+  ]
+}
