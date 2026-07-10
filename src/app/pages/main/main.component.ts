9a273bb5956fdf4b371474a36c07c920e67a6683
Sergey Malyuga <sergey@malyuga.ru>
2026-07-10 09:59:38 +0300

    feat(home-page): добавить смену города

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -4,16 +4,26 @@ import {Store} from '@ngrx/store';
 import {AppState} from '../../core/models/app.state';
 import {selectCurrentCity, selectOffersByCity} from '../../store/app/selectors/app.selectors';
 import {OfferCardComponent} from '../../shared/components/offer-card/offer-card.component';
+import {CITY_LOCATIONS} from '../../core/constants/const';
+import {NgClass} from '@angular/common';
+import {City} from '../../core/models/city';
+import {changeCity} from '../../store/city/actions/city.actions';
 
 @Component({
   selector: 'app-main',
-  imports: [HeaderComponent, OfferCardComponent],
+  imports: [HeaderComponent, OfferCardComponent, NgClass],
   templateUrl: './main.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MainComponent {
   private store = inject(Store<AppState>);
 
+  protected readonly CITY_LOCATIONS = CITY_LOCATIONS;
+
   public offers = this.store.selectSignal(selectOffersByCity);
   public currentCity = this.store.selectSignal(selectCurrentCity);
+
+  public changeCity(city: City) {
+    this.store.dispatch(changeCity({city}));
+  }
 }
