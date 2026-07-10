c1747719cd97e81351c3e7110208ed679c5979fa
Sergey Malyuga <sergey@malyuga.ru>
2026-07-10 13:45:29 +0300

    feat(home-page): добавить смену активного оффера

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,16 +1,14 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { HeaderComponent } from '../../shared/components/header/header.component';
-import { Store } from '@ngrx/store';
-import { AppState } from '../../core/models/app.state';
-import {
-  selectCurrentCity,
-  selectOffersByCity,
-} from '../../store/app/selectors/app.selectors';
-import { OfferCardComponent } from '../../shared/components/offer-card/offer-card.component';
-import { CITY_LOCATIONS } from '../../core/constants/const';
-import { NgClass } from '@angular/common';
-import { City } from '../../core/models/city';
-import { changeCity } from '../../store/city/actions/city.actions';
+import {ChangeDetectionStrategy, Component, inject, signal} from '@angular/core';
+import {HeaderComponent} from '../../shared/components/header/header.component';
+import {Store} from '@ngrx/store';
+import {AppState} from '../../core/models/app.state';
+import {selectCurrentCity, selectOffersByCity,} from '../../store/app/selectors/app.selectors';
+import {OfferCardComponent} from '../../shared/components/offer-card/offer-card.component';
+import {CITY_LOCATIONS} from '../../core/constants/const';
+import {NgClass} from '@angular/common';
+import {City} from '../../core/models/city';
+import {changeCity} from '../../store/city/actions/city.actions';
+import {OfferPreview} from '../../core/models/offers';
 
 @Component({
   selector: 'app-main',
@@ -25,8 +23,13 @@ export class MainComponent {
 
   public offers = this.store.selectSignal(selectOffersByCity);
   public currentCity = this.store.selectSignal(selectCurrentCity);
+  public activeCard = signal<OfferPreview | null>(null);
 
   public changeCity(city: City) {
-    this.store.dispatch(changeCity({ city }));
+    this.store.dispatch(changeCity({city}));
+  }
+
+  public changeActiveCard(offer: OfferPreview | null) {
+    this.activeCard.set(offer);
   }
 }
