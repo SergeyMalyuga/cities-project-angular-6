339d217c2b97045b27b38f302ef7d20ba2407184
Sergey Malyuga <sergey@malyuga.ru>
2026-07-10 11:19:13 +0300

    feat(user): создать состояние пользователя

---
+++ src/app/store/user/selectors/user.selectors.ts
@@ -0,0 +1,14 @@
+import {createFeatureSelector, createSelector} from '@ngrx/store';
+import {AppState} from '../../../core/models/app.state';
+
+const selectUserState = createFeatureSelector<AppState['user']>('user');
+
+export const selectAuthStatus = createSelector(
+  selectUserState,
+  state => state.authorizationStatus
+  );
+
+export const selectEmail = createSelector(
+  selectUserState,
+  state => state.user?.email
+);
---
+++ src/app/store/user/user.reducer.ts
@@ -0,0 +1,49 @@
+import {UserState} from '../../core/models/user.state';
+import {AuthorizationStatus, DEFAULT_USER} from '../../core/constants/const';
+import {createReducer, on} from '@ngrx/store';
+import {
+  checkAuth,
+  checkAuthFailure,
+  checkAuthSuccess,
+  login, loginFailure,
+  loginSuccess, logout, logoutFailure,
+  logoutSuccess
+} from './actions/user.actions';
+
+const initialState: UserState = {
+  user: DEFAULT_USER,
+  authorizationStatus: AuthorizationStatus.UNKNOWN,
+  isLoading: false,
+  error: null,
+}
+
+export const userReducer = createReducer(
+  initialState,
+  on(checkAuth, state => ({
+    ...state, isLoading: true
+  })),
+  on(checkAuthSuccess, (state, {user}) => ({
+    ...state, user, authorizationStatus: AuthorizationStatus.AUTH, isLoading: false, error: null
+  })),
+  on(checkAuthFailure, (state, {error}) => ({
+    ...state, authorizationStatus: AuthorizationStatus.UN_AUTH, isLoading: false, error
+  })),
+  on(login, state => ({
+    ...state, isLoading: true
+  })),
+  on(loginSuccess, (state, {user}) => ({
+    ...state, user, authorizationStatus: AuthorizationStatus.AUTH, isLoading: false, error: null
+  })),
+  on(loginFailure, (state, {error}) => ({
+    ...state, authorizationStatus: AuthorizationStatus.UN_AUTH, isLoading: false, error
+  })),
+  on(logout, state => ({
+    ...state, isLoading: true
+  })),
+  on(logoutSuccess, state => ({
+    ...state, user: DEFAULT_USER, authorizationStatus: AuthorizationStatus.UN_AUTH, isLoading: false, error: null
+  })),
+  on(logoutFailure, (state, {error}) => ({
+    ...state, isLoading: false, error
+  }))
+);
