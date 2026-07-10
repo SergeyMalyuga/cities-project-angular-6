36c4bd67652a2eab53bfc8eb626624f0eebc9927
Sergey Malyuga <sergey@malyuga.ru>
2026-07-10 11:45:47 +0300

    feat(header): настроить интерфейс в зависимости от статуса аутентификации

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,9 +1,24 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {Store} from '@ngrx/store';
+import {AppState} from '../../../core/models/app.state';
+import {selectAuthStatus, selectEmail} from '../../../store/user/selectors/user.selectors';
+import {RouterLink} from '@angular/router';
+import {AppRoute} from '../../../core/constants/const';
+import {isAuth} from '../../../core/utils/auth-status';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './header.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  private store = inject(Store<AppState>);
+  protected readonly AppRoute = AppRoute;
+
+  public authStatus = this.store.selectSignal(selectAuthStatus);
+  public userEmail = this.store.selectSignal(selectEmail);
+  protected readonly isAuth = isAuth;
+}
