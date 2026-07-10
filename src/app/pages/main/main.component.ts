58895590dcfde4e12831de40089437f010828252
Sergey Malyuga <sergey@malyuga.ru>
2026-07-10 14:09:44 +0300

    feat(home-page): добавить карту с локацией офферов

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -9,10 +9,11 @@ import {NgClass} from '@angular/common';
 import {City} from '../../core/models/city';
 import {changeCity} from '../../store/city/actions/city.actions';
 import {OfferPreview} from '../../core/models/offers';
+import {MapComponent} from '../../shared/components/map/map.component';
 
 @Component({
   selector: 'app-main',
-  imports: [HeaderComponent, OfferCardComponent, NgClass],
+  imports: [HeaderComponent, OfferCardComponent, NgClass, MapComponent],
   templateUrl: './main.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
