495314a1295635f2db193410651f2aae6a1f5352
Sergey Malyuga <sergey@malyuga.ru>
2026-07-10 09:44:11 +0300

    feat(home-page): отобразить офферы выбранного города

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,10 +1,19 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { HeaderComponent } from '../../shared/components/header/header.component';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {HeaderComponent} from '../../shared/components/header/header.component';
+import {Store} from '@ngrx/store';
+import {AppState} from '../../core/models/app.state';
+import {selectCurrentCity, selectOffersByCity} from '../../store/app/selectors/app.selectors';
+import {OfferCardComponent} from '../../shared/components/offer-card/offer-card.component';
 
 @Component({
   selector: 'app-main',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, OfferCardComponent],
   templateUrl: './main.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MainComponent {}
+export class MainComponent {
+  private store = inject(Store<AppState>);
+
+  public offers = this.store.selectSignal(selectOffersByCity);
+  public currentCity = this.store.selectSignal(selectCurrentCity);
+}
