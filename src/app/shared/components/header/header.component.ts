40ff4a18fa144996ae111a5da61a83829a724c11
Sergey Malyuga <sergey@malyuga.ru>
2026-07-14 11:32:39 +0300

    feat(header): добавить выход из учётной записи

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,13 +1,14 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { AppState } from '../../../core/models/app.state';
+import {ChangeDetectionStrategy, Component, effect, inject} from '@angular/core';
+import {Store} from '@ngrx/store';
+import {AppState} from '../../../core/models/app.state';
 import {
   selectAuthStatus,
   selectEmail,
 } from '../../../store/user/selectors/user.selectors';
-import { RouterLink } from '@angular/router';
-import { AppRoute } from '../../../core/constants/const';
-import { isAuth } from '../../../core/utils/auth-status';
+import {Router, RouterLink} from '@angular/router';
+import {AppRoute, AuthorizationStatus} from '../../../core/constants/const';
+import {isAuth} from '../../../core/utils/auth-status';
+import {logout} from '../../../store/user/actions/user.actions';
 
 @Component({
   selector: 'app-header',
@@ -17,9 +18,17 @@ import { isAuth } from '../../../core/utils/auth-status';
 })
 export class HeaderComponent {
   private store = inject(Store<AppState>);
+  private router = inject(Router);
+
   protected readonly AppRoute = AppRoute;
+  protected readonly isAuth = isAuth;
 
   public authStatus = this.store.selectSignal(selectAuthStatus);
   public userEmail = this.store.selectSignal(selectEmail);
-  protected readonly isAuth = isAuth;
+
+  public logout() {
+    if (this.isAuth(this.authStatus())) {
+      this.store.dispatch(logout());
+    }
+  }
 }
