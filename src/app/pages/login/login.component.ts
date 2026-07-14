7e8030d1f346c91f4c949e1a3bfe6b41a87d1c1e
Sergey Malyuga <sergey@malyuga.ru>
2026-07-14 11:34:27 +0300

    feat(login): добавить возможность аутентификации
    - Настроить валидацию полей формы

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,9 +1,59 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, DestroyRef, inject, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {Credentials} from '../../core/models/credentials';
+import {Store} from '@ngrx/store';
+import {AppState} from '../../core/models/app.state';
+import {selectAuthStatus} from '../../store/user/selectors/user.selectors';
+import {first} from 'rxjs';
+import {AppRoute, AuthorizationStatus} from '../../core/constants/const';
+import {Router} from '@angular/router';
+import {loadOffers} from '../../store/offer/actions/offer.actions';
+import {login} from '../../store/user/actions/user.actions';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+  private store = inject(Store<AppState>);
+  private router = inject(Router);
+  private destroyRef = inject(DestroyRef);
+  private fb = inject(FormBuilder);
+
+  public loginForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.pattern('^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]+$')]],
+  });
+
+  public get emailControl() {
+    return this.loginForm.get('email');
+  }
+
+  public get passwordControl() {
+    return this.loginForm.get('password');
+  }
+
+  public onSubmit() {
+    if (this.loginForm.valid) {
+      const {email, password} = this.loginForm.value;
+      const credentials: Credentials = {email, password};
+      this.store.dispatch(login({credentials}));
+    }
+  }
+
+  public ngOnInit(): void {
+    this.store.select(selectAuthStatus).pipe(
+      first(authStatus => authStatus === AuthorizationStatus.AUTH),
+      takeUntilDestroyed(this.destroyRef))
+      .subscribe(() => {
+        this.store.dispatch(loadOffers());
+        this.loginForm.reset();
+        this.router.navigate([AppRoute.MAIN]);
+      })
+  }
+}
