90d23e695b74159b2cb9ec6dc8d419de8ca34a01
Sergey Malyuga <sergey@malyuga.ru>
2026-07-14 11:31:59 +0300

    feat(app.config): подключить AuthInterceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,23 +1,30 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
 import {
+  HTTP_INTERCEPTORS,
   provideHttpClient,
   withInterceptorsFromDi,
 } from '@angular/common/http';
-import { provideStore } from '@ngrx/store';
-import { appReducer } from './store/app/app.reducer';
-import { provideEffects } from '@ngrx/effects';
-import { OfferEffects } from './store/offer/effects/offer.effects';
-import { UserEffects } from './store/user/effects/user.effects';
+import {provideStore} from '@ngrx/store';
+import {appReducer} from './store/app/app.reducer';
+import {provideEffects} from '@ngrx/effects';
+import {OfferEffects} from './store/offer/effects/offer.effects';
+import {UserEffects} from './store/user/effects/user.effects';
+import {AuthInterceptor} from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
     provideStore(appReducer),
     provideEffects(OfferEffects, UserEffects),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
   ],
 };
