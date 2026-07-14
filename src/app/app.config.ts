d63af13e6fde9ed704c1deabc5f6c051c69eef3e
Sergey Malyuga <sergey@malyuga.ru>
2026-07-14 15:08:54 +0300

    feat(app.config): подключить эффекты избранных офферов

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -13,6 +13,7 @@ import {provideEffects} from '@ngrx/effects';
 import {OfferEffects} from './store/offer/effects/offer.effects';
 import {UserEffects} from './store/user/effects/user.effects';
 import {AuthInterceptor} from './core/interceptors/auth.interceptor';
+import {FavoriteOfferEffects} from './store/favorite-offer/effects/favorite-offer.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -20,7 +21,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
     provideStore(appReducer),
-    provideEffects(OfferEffects, UserEffects),
+    provideEffects(OfferEffects, UserEffects, FavoriteOfferEffects),
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
