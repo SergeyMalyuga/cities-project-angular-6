7d747ac87e2db6428d085590ab6cbec41e023040
Sergey Malyuga <sergey@malyuga.ru>
2026-07-14 11:29:15 +0300

    feat(app.routes): добавить authGuard для защищенных маршрутов

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
-import { Routes } from '@angular/router';
-import { AppRoute } from './core/constants/const';
+import {Routes} from '@angular/router';
+import {AppRoute} from './core/constants/const';
+import {AuthGuard} from './core/guards/auth.guard';
 
 export const routes: Routes = [
   {
@@ -19,6 +20,7 @@ export const routes: Routes = [
   {
     path: `${AppRoute.OFFER}/:id`,
     title: 'Offer',
+    canActivate: [AuthGuard],
     loadComponent: () =>
       import('./pages/offer/offer.component').then((m) => m.OfferComponent),
   },
