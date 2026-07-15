6c14e709bfdacc85e06789a173395dceb0e7cf57
Sergey Malyuga <sergey@malyuga.ru>
2026-07-15 11:49:34 +0300

    feat(offer): добавить загрузку оффера
    - Отобразить данные оффера

--- src/app/pages/offer/offer.component.ts
+++ src/app/pages/offer/offer.component.ts
@@ -1,10 +1,98 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { HeaderComponent } from '../../shared/components/header/header.component';
+import {ChangeDetectionStrategy, Component, computed, DestroyRef, inject, OnInit, signal} from '@angular/core';
+import {HeaderComponent} from '../../shared/components/header/header.component';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Offer, OfferPreview} from '../../core/models/offers';
+import {catchError, combineLatest, EMPTY, filter, map, merge, of, Subject, switchMap, tap} from 'rxjs';
+import {Comment} from '../../core/models/comments';
+import {OfferDataService} from '../../core/services/offer-data.service';
+import {AppRoute} from '../../core/constants/const';
+import {NgClass, TitleCasePipe} from '@angular/common';
+import {isEnableActiveMark} from '../../core/utils/enable-active-mark';
+import {Store} from '@ngrx/store';
+import {AppState} from '../../core/models/app.state';
+import {selectAuthStatus} from '../../store/user/selectors/user.selectors';
+import {OfferService} from '../../core/services/offer.service';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import {getRatingWidth} from '../../core/utils/rating-width';
 
 @Component({
   selector: 'app-offer',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, NgClass, TitleCasePipe],
   templateUrl: './offer.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class OfferComponent {}
+export class OfferComponent implements OnInit {
+  private activatedRouter = inject(ActivatedRoute);
+  private refreshOffer$ = new Subject<void>();
+  private refreshComments$ = new Subject<void>();
+  private refreshNearbyOffers$ = new Subject<void>();
+  private offerDataService = inject(OfferDataService);
+  private offerService = inject(OfferService);
+  private router = inject(Router);
+  private store = inject(Store<AppState>);
+  private destroyRef = inject(DestroyRef);
+
+  protected readonly isEnableActiveMark = isEnableActiveMark;
+  protected readonly getRatingWidth = getRatingWidth;
+
+  public offer = signal<Offer | null>(null);
+  public comments = signal<Comment[]>([]);
+  public nearbyOffers = signal<OfferPreview[]>([]);
+  public authStatus = this.store.selectSignal(selectAuthStatus);
+  public isToggleStatus = signal<boolean>(false);
+  public readonly bedrooms = computed(() => this.offer()?.bedrooms ?? 0);
+  public readonly maxAdults = computed(() => this.offer()?.maxAdults ?? 0);
+
+  ngOnInit(): void {
+    this.activatedRouter.paramMap.pipe(map(params => params.get('id')),
+      filter((id): id is string => id !== null),
+      switchMap(id => {
+        const offer$ = merge(
+          this.offerDataService.getOfferById(id),
+          this.refreshOffer$.pipe(
+            switchMap(() => this.offerDataService.getOfferById(id).pipe(
+                catchError(() => {
+                  this.router.navigate([AppRoute.MAIN]);
+                  return EMPTY
+                }),
+              )
+            ))
+        );
+
+        const nearbyOffers$ = merge(
+          this.offerDataService.getNearbyOffers(id),
+          this.refreshNearbyOffers$.pipe(switchMap(() => this.offerDataService.getNearbyOffers(id)
+            .pipe(catchError(() => of([])))),
+          )
+        );
+
+        return combineLatest({offer: offer$, nearbyOffers: nearbyOffers$})
+      }), takeUntilDestroyed(this.destroyRef)).subscribe(result => {
+      this.offer.set(result.offer);
+      this.nearbyOffers.set(result.nearbyOffers);
+      this.isToggleStatus.set(false);
+    })
+  }
+
+  public toggleFavoriteStatus() {
+    const offer = this.offer();
+    if (offer) {
+      this.isToggleStatus.set(true);
+      this.offerService.toggleFavorite(offer.id, !offer.isFavorite).pipe(
+        filter((success) => success !== null),
+        tap((success) => {
+          if (success) {
+            this.refreshOffer$.next();
+          } else {
+            this.isToggleStatus.set(false);
+          }
+        }),
+        catchError(() => {
+          this.isToggleStatus.set(false);
+          return EMPTY;
+        }),
+        takeUntilDestroyed(this.destroyRef),
+      ).subscribe();
+    }
+  }
+}
