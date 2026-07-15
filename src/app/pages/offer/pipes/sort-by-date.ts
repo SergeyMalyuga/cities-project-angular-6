a83c521f1df6630d1cc70e93d235dd0b313e66cf
Sergey Malyuga <sergey@malyuga.ru>
2026-07-15 14:12:41 +0300

    feat(shared): добавить pipe для сортировки офферов по дате

---
+++ src/app/pages/offer/pipes/sort-by-date.ts
@@ -0,0 +1,23 @@
+import {Pipe, PipeTransform} from '@angular/core';
+import {Comment} from '../../../core/models/comments';
+
+@Pipe({
+  name: 'sortByDate'
+})
+export class SortByDatePipe implements PipeTransform {
+  transform(comments: Comment[]): Comment[] {
+    if (!comments || comments.length === 0) {
+      return [];
+    }
+
+    return [...comments].sort((a, b) => {
+      const dataA = new Date(a.date).getTime();
+      const dataB = new Date(b.date).getTime();
+
+      const validDataA = Number.isNaN(dataA) ? 0 : dataA;
+      const validDataB = Number.isNaN(dataA) ? 0 : dataB;
+
+      return validDataB - validDataA
+    })
+  }
+}
