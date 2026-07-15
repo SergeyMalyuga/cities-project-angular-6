91e306093392ec938f53ecf41cea12bbadbb02f0
Sergey Malyuga <sergey@malyuga.ru>
2026-07-15 14:54:02 +0300

    feat(scroll-up): добавить директиву прокрутки страницы

---
+++ src/app/shared/directives/scroll-up.directive.ts
@@ -0,0 +1,27 @@
+import {Directive, HostListener, inject, Input} from '@angular/core';
+import {DOCUMENT} from '@angular/common';
+
+@Directive({
+  selector: '[appScrollUp]',
+})
+export class ScrollUpDirective {
+  @Input() enable = false;
+
+  private document = inject(DOCUMENT);
+  private windowRef = this.document.defaultView;
+
+  @HostListener('click')
+  onClick() {
+    if (this.enable && this.windowRef) {
+      this.windowRef.scrollTo({top: 0, behavior: 'smooth'});
+    }
+  }
+
+  @HostListener('keydown', ['$event'])
+  onKeyDown(evt: KeyboardEvent) {
+    if (this.enable && this.windowRef && (evt.key === 'Enter' || evt.key === ' ')) {
+      evt.preventDefault();
+      this.windowRef.scrollTo({top: 0, behavior: 'smooth'});
+    }
+  }
+}
