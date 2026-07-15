3a3c8c70272cacc651522235ddab967e15a72dde
Sergey Malyuga <sergey@malyuga.ru>
2026-07-15 14:55:22 +0300

    feat(offer-card): добавить кастомную блокировку кнопки favorite

--- src/app/shared/components/offer-card/offer-card.component.ts
+++ src/app/shared/components/offer-card/offer-card.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, inject, Input, Output, signal} from '@angular/core';
+import {ChangeDetectionStrategy, Component, DestroyRef, EventEmitter, inject, Input, Output} from '@angular/core';
 import {OfferPreview} from '../../../core/models/offers';
 import {getRatingWidth} from '../../../core/utils/rating-width';
 import {NgClass, TitleCasePipe} from '@angular/common';
@@ -6,11 +6,13 @@ import {HoverTrackerDirective} from '../../directives/hover-tracker.directive';
 import {AppState} from '../../../core/models/app.state';
 import {Store} from '@ngrx/store';
 import {selectAuthStatus} from '../../../store/user/selectors/user.selectors';
-import {AppRoute, AuthorizationStatus} from '../../../core/constants/const';
+import {AppRoute} from '../../../core/constants/const';
 import {OfferService} from '../../../core/services/offer.service';
 import {selectIsFavoriteOffersLoading} from '../../../store/favorite-offer/selectors/favorite-offers.selectors';
 import {RouterLink} from '@angular/router';
 import {isEnableActiveMark} from '../../../core/utils/enable-active-mark';
+import {first, tap} from 'rxjs';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-offer-card',
@@ -21,10 +23,13 @@ import {isEnableActiveMark} from '../../../core/utils/enable-active-mark';
 export class OfferCardComponent {
   @Input({required: true}) offer!: OfferPreview;
   @Input() isHoverTrackerEnable = false;
+  @Input() isDisabled: boolean | null = null;
   @Output() hovered = new EventEmitter<OfferPreview | null>();
+  @Output() toggled = new EventEmitter<void>();
 
   private store = inject(Store<AppState>);
   private offerService = inject(OfferService);
+  private destroyRef = inject(DestroyRef);
 
   protected readonly AppRoute = AppRoute;
   protected readonly isEnableActiveMark = isEnableActiveMark;
@@ -43,6 +48,9 @@ export class OfferCardComponent {
   }
 
   public toggleFavoriteStatus() {
-    this.offerService.toggleFavorite(this.offer.id, !this.offer.isFavorite);
+    this.offerService.toggleFavorite(this.offer.id, !this.offer.isFavorite)
+      .pipe(first(success => success !== null),
+        tap(() => this.toggled.emit()),
+        takeUntilDestroyed(this.destroyRef)).subscribe();
   }
 }
